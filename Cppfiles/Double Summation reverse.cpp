7683c7978789a0751b73ad16786f6bbbf232eb8f
Nalin Shani <nalinshani14@gmail.com>
2016-08-10 21:50:08 +0530

    Add files via upload
    When you learn double summation (smiley)

---
+++ Cppfiles/Double Summation reverse.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int x,sum=0;
+    cout<<"Enter limit\n";
+    cin>>x;
+    for(int j=0;j<=x;j++)
+    {
+        for(int i=j;i<=x;i++)
+        sum=sum+(i+j)*(i*j);
+    }
+    cout<<"Sum="<<sum;
+}
---
+++ Cppfiles/Double Summation.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int x,sum=0;
+    cout<<"Enter limit\n";
+    cin>>x;
+    for(int i=0;i<=x;i++)
+    {
+        for(int j=0;j<=i;j++)
+        sum=sum+(i+j)*(i*j);
+    }
+    cout<<"Sum="<<sum;
+}
