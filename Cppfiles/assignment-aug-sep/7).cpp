07104bc60a917ab5fa86bf1c2688a57eddc2b53c
Nalin Shani <nalinshani14@gmail.com>
2016-08-31 22:35:02 +0530

    Update 7).cpp
    This is better as i will be editing the same file for lcm

--- Cppfiles/assignment-aug-sep/7).cpp
+++ Cppfiles/assignment-aug-sep/7).cpp
@@ -1,19 +1,22 @@
 #include<iostream>
-#include<conio.h>
 using namespace std;
 int main()
 {
     cout<<"Enter two no.\n";
-    int x1,x2,hcf,flag=0;
+    int x1,x2,hcf,lcm,flag=0;
     cin>>x1;
     cin>>x2;
-    for(int i=1;i<=max(x1,x2);i++)
+    for(int i=2;i<=max(x1,x2);i++)
     {
         if(x1%i==0&&x2%i==0)
-            hcf=i; flag=1;
+            {hcf=i; flag=1;}
     }
     if(flag==0)
-        cout<<"HCF is 1";
-    if(flag==1)
-        cout<<"HCF is "<<hcf;
+        {
+            cout<<"HCF is 1";
+        }
+    else if(flag==1)
+        {
+            cout<<"HCF is "<<hcf;
+        }
 }
