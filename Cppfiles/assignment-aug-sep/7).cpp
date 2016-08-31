121bc08ec9e1026b037eb4579b01b347d0c0a0a2
Nalin Shani <nalinshani14@gmail.com>
2016-08-31 22:59:50 +0530

    Update 7).cpp
    FINALLY!!!!!!!!!!!!

--- Cppfiles/assignment-aug-sep/7).cpp
+++ Cppfiles/assignment-aug-sep/7).cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
     cout<<"Enter two no.\n";
-    int x1,x2,hcf,lcm,flag=0;
+    int x1,x2,hcf,flag=0;
     cin>>x1;
     cin>>x2;
     for(int i=1;i<=max(x1,x2);i++)
@@ -14,9 +14,11 @@ int main()
     if(flag==0)
         {
             cout<<"HCF is 1";
+            hcf=1;
         }
     else if(flag==1)
         {
             cout<<"HCF is "<<hcf;
         }
+    cout<<"\nLCM="<<(float)((x1*x2)/hcf);
 }
