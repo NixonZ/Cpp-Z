5eb7a842fda9bc8046bc4517923b7b50ba905867
Nalin Shani <nalinshani14@gmail.com>
2016-09-01 20:33:10 +0530

    Update 1)i.cpp

--- Cppfiles/assignment-aug-sep/1)i.cpp
+++ Cppfiles/assignment-aug-sep/1)i.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<conio.h>
 #include<math.h>
 using namespace std;
 int main()
@@ -8,17 +9,17 @@ int main()
     cin>>x;
     cout<<"Enter the no. of terms\n";
     cin>>y;
-    for(int i=2;i<=2*y;i=i+2)
+    for(int i=1,j=1;i<=2*y,j<=y;i=i+2,j++)
     {
-        if(i%4==0)
+        if(j%2)
            {
             z=(1.0)/(pow(x,i));
-            cout<<'-'<<"1/"<<x<<'^'<<i;
+            cout<<'+'<<"1/"<<x<<'^'<<i;
            }
         else
         {
             z=(-1)*(1.0)/(pow(x,i));
-            cout<<'+'<<"1/"<<x<<'^'<<i;
+            cout<<'-'<<"1/"<<x<<'^'<<i;
         }
     }
 }
