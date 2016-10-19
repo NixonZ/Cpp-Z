c6b67f777e4ae6603778307d301e81d8c0714ec9
Nalin Shani <nalinshani14@gmail.com>
2016-10-19 16:08:03 +0530

    Update Q2.cpp

--- Cppfiles/Array-Assignment/1-D/Q2.cpp
+++ Cppfiles/Array-Assignment/1-D/Q2.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main()
 {
     int A[10];
-    int sum=0,sum7=0,sump=0,sume1=0,sumo1=0,maxm,minm,sume2=0,sumo2=0;
-    for(int i=0;i<10;i++)
+    int k,i,j,sum=0,sum7=0,sump=0,sume1=0,sumo1=0,maxm,minm,sume2=0,sumo2=0;
+    for(i=0;i<10;i++)
     {
         cout<<"A["<<i+1<<"]:";
         cin>>A[i];
@@ -39,13 +39,13 @@ int main()
         <<"Sum of Multiples of 7="<<sum7;
     int avg=sum/10,counta=0,countb=0,B[10],l=0;
     cout<<"\nPrime no. are\n";
-    for(int i=0;i<10;i++)
+    for(i=0;i<10;i++)
     {
         if(A[i]>avg)
             counta++;
         else
                 countb++;
-        for(int k=2;k<A[i];k++)
+        for(k=2;k<A[i];k++)
         {
                 if(!(A[i]%k))
                     l=1;
@@ -56,13 +56,13 @@ int main()
     cout<<"\nCount of no. above average=" <<counta
         <<"\nCount of no. below average=" <<countb;
     int temp;
-    for(int i=-1,k=10;i<5;k--,i++)
+    for(i=0,k=9;i<5;k--,i++)
     {
         temp=A[i];
         A[i]=A[k];
         A[k]=temp;
     }
     cout<<"\nReverse\n";
-    for(int i=0;i<10;i++)
-        cout<<A[i]<<'\t';
+    for(i=0;i<10;i++)
+        cout<<A[i]<<endl;
 }
