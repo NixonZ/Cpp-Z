04b64ef8e18250c8fc0951522ca63aa80792817f
Nalin Shani <nalinshani14@gmail.com>
2016-12-17 14:58:27 +0530

    Update Q3.cpp

--- Cppfiles/Assignment-Dec/Assignment-structures/Q3.cpp
+++ Cppfiles/Assignment-Dec/Assignment-structures/Q3.cpp
@@ -9,9 +9,9 @@ int check_date(date c)
 {
     if((c.mm>0&&c.mm<=12)&&(c.dd>0&&c.dd<=31)&&c.yy>0)
     {
-        if(c.mm%2==0&&c.dd>30)
+        if(c.mm%2==0&&c.dd>30)//month c
             return 0;
-        else if(c.mm==2&&c.dd>28)
+        else if(c.mm==2&&c.dd>28)//feb check
         {
             if(c.dd==29&&c.yy%4==0)
                 return 1;
@@ -61,18 +61,21 @@ int main()
     cout<<"Enter the date\n";
     date_in(d1);
     (check_date(d1))?cout<<"The date is correct\n":cout<<"The date is not correct\n";
+    do
+    {
+    y=0;
     cout<<"Enter two dates\n";
     cout<<"Date 1\n";
     date_in(d[0]);
     cout<<"Date 2\n";
     date_in(d[1]);
-    if(!check_date(d[0])&&!check_date(d[1]))
+    if(!check_date(d[0])||!(check_date(d[1])))
     {
         cout<<"one of the date is wrong\n";
         y=1;
     }
     int x=date_cmp(d[0],d[1]);
-    if(y=1);
+    if(y==1);
     else if(x==7)
     {
         cout<<"The dates are equal and are\n";
@@ -81,9 +84,10 @@ int main()
     else
     {
         cout<<"The later date is\n";
-        if(date_cmp(d[0],d[1]))
+        if(x)
             date_out(d[0]);
         else
             date_out(d[1]);
     }
+    }while(y==1);
 }
