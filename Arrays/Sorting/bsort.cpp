dcbbc25f37090533c3885499fd1ee97a30571e38
NixonZ <nalinshani14@gmail.com>
2017-09-10 20:06:52 +0530

    Added limiting to bubble sort
    Auto exit if array sorted

--- Arrays/Sorting/bsort.cpp
+++ Arrays/Sorting/bsort.cpp
@@ -1,9 +1,10 @@
 #include"../main.h"
 void bsort(int A[], int T)
 {
-    int temp;
+    int temp,flag;
     for(int i=0;i<T;i++)
     {
+        flag=1;
         for(int j=0;j<T-1;j++)
         {
             if(A[j]>A[j+1])
@@ -11,7 +12,10 @@ void bsort(int A[], int T)
                 temp=A[j];
                 A[j]=A[j+1];
                 A[j+1]=temp;
+                flag=0;
             }
         }
+        if(flag)
+            break;
     }
 }
